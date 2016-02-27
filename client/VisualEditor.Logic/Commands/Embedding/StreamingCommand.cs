b774a669d15e5ce8f2fab52ee70909c7b355707f
Frame_Bassman <Framebassman@gmail.com>
2016-02-27 16:58:11 +0300

    Initial implementation of link processing.

--- client/VisualEditor.Logic/Commands/Embedding/StreamingCommand.cs
+++ client/VisualEditor.Logic/Commands/Embedding/StreamingCommand.cs
@@ -1,12 +1,19 @@
 ﻿namespace VisualEditor.Logic.Commands.Embedding
 {
+    using System;
+    using System.IO;
     using System.Windows.Forms;
 
     using VisualEditor.Logic.Dialogs;
+    using VisualEditor.Logic.Helpers;
     using VisualEditor.Logic.Warehouse;
+    using VisualEditor.Utils.Controls.HtmlEditing;
+    using VisualEditor.Utils.ExceptionHandling;
 
     internal class StreamingCommand : AbstractCommand
     {
+        private const string operationCantBePerformedMessage = "Невозможно вставить видео в редактор.";
+
         public StreamingCommand()
         {
             this.name = CommandNames.StreamingCommand;
@@ -39,7 +46,31 @@
 
                 if (streamingVideoDialog.ShowDialog(EditorObserver.DialogOwner) == DialogResult.OK)
                 {
-                    
+                    var rawHtml = EditorObserver.ActiveEditor.Document.CreateElement(TagNames.ImageTagName);
+
+                    rawHtml.SetAttribute("sdocument", "0");
+
+                    #region Источник
+
+                    var sourceImage = Path.Combine(Warehouse.RelativeImagesDirectory, "Vid.png");
+                    rawHtml.SetAttribute("src", sourceImage);
+
+                    var sourceLink = dataTransferUnit.GetNodeValue("Source");
+                    rawHtml.SetAttribute("src_", sourceLink);
+
+                    #endregion
+
+                    try
+                    {
+                        HtmlEditingToolHelper.InsertHtml(EditorObserver.ActiveEditor, rawHtml);
+                    }
+                    catch (Exception exception)
+                    {
+                        ExceptionManager.Instance.LogException(exception);
+                        UIHelper.ShowMessage(operationCantBePerformedMessage, MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        return;
+                    }
                 }
             }
         }
